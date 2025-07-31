4bde7594a4231ed573508e42bd48b5415e3add72
Mohamed Rabea <130266303+MohamedAbuelroos@users.noreply.github.com>
2025-07-31 13:49:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector("#verify");
+const input = document.querySelector("#code");
+const error = document.querySelector("#error");
+
+const code = "MWRBP-MKMT3-T5593-L95K8-C2F6E";
+let verified = false;
+
+function verify() {
+  if (input.value === "") {
+    error.textContent = "يجب ألا يكون الحقل فارغًا";
+  } else if (input.value.length < 25 || input.value !== code) {
+    error.textContent = "كود غير صالح";
+  } else {
+    error.textContent = "";
+    document.body.classList.add("loading");
+
+    setTimeout(() => {
+      document.body.classList.remove("loading");
+
+      window.open("./MWRBPMKMT3T5593L95K8C2F6E.pdf", "_blank");
+    }, 3000);
+    input.value = "";
+  }
+}
+btn.addEventListener("click", () => {
+  verify();
+});
